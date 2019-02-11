6cae1c6ef7bb623e4ed3e478f87f673c2a39fd6e
r-a-h-u-l-m <47514157+r-a-h-u-l-m@users.noreply.github.com>
2019-02-11 10:42:40 +0530

    Update and rename README.md to o.c

---
+++ o.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int b;
+    printf("enter a number");
+    scanf("%d",&b);
+    
+    if(b%2==0)
+    {
+        printf("even number");
+    }
+    else 
+    {
+        printf("odd number");
+    }
+    getch();
+}
+    
